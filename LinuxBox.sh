5ded6f764b4a208445187a6530dc49d08516b435
赵博 <59049820+666zhaobo666@users.noreply.github.com>
2025-08-24 18:37:58 +0800

    Update version and proxy URL in LinuxBox.sh

--- LinuxBox.sh
+++ LinuxBox.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # LinuxBox 多功能管理脚本
 #版本信息
-version="2.0.0"
+version="2.0.1"
 ## 全局颜色变量
 white='\033[0m'			# 白色
 green='\033[0;32m'		# 绿色
@@ -55,7 +55,7 @@ use_proxy(){
     detect_region
     
     if [ "$region" == "CN" ]; then
-        url_proxy="http://107.150.27.238:8046/"
+        url_proxy="https://proxy.cccg.top/"
     else
         url_proxy="https://"
     fi
