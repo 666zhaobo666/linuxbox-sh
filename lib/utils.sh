eb7b7794bb3760b5232299e3c21ca0e241244573
Joey <1606926099@qq.com>
2026-06-02 22:35:42 +0800

    fix update logic

--- lib/utils.sh
+++ lib/utils.sh
@@ -71,4 +71,34 @@ fi
 
 # ipv4_address=$(curl -s https://ipinfo.io/ip && echo)
 ipv6_address=$(curl -s --max-time 1 https://v6.ipinfo.io/ip && echo)
+
+##  通用下载函数 (无 shebang 验证, lib/ 下的库文件首行不一定是 shebang)
+# 用法: download_file <remote_url> <local_path>
+# 成功返回 0, 失败返回 1
+download_file() {
+	local url="$1"
+	local path="$2"
+	if curl -sSL --max-time 60 --fail "$url" -o "$path" 2>/dev/null; then
+		[ -s "$path" ] && return 0
+	fi
+	return 1
+}
+
+##  进度条渲染 (覆盖式, 用 \r 回到行首)
+# 用法: show_progress <current> <total> [bar_width]
+show_progress() {
+	local current=$1
+	local total=$2
+	local width=${3:-30}
+	local pct=$(( current * 100 / total ))
+	local filled=$(( current * width / total ))
+	local empty=$(( width - filled ))
+
+	local bar=""
+	local i
+	for ((i=0; i<filled; i++)); do bar+="█"; done
+	for ((i=0; i<empty; i++)); do bar+="░"; done
+
+	printf "\r${cyan}下载中: [${green}%s${cyan}] %3d%% (%d/%d)${white}" "$bar" "$pct" "$current" "$total"
+}
 }
