a35ff6c6215bcf017f321a3ef378d27e783cce95
Joey <1606926099@qq.com>
2026-06-13 13:47:19 +0800

    chore: bump version to 3.3.2

---
+++ bump_version.py
@@ -0,0 +1,9 @@
+﻿import re
+
+with open("LinuxBox.sh", "r", encoding="utf-8") as f:
+    content = f.read()
+
+content = re.sub(r'^version="3\.3\.1"', 'version="3.3.2"', content, flags=re.MULTILINE)
+
+with open("LinuxBox.sh", "w", encoding="utf-8") as f:
+    f.write(content)
